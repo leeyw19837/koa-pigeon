6fab5e2f34cb1b2461dd8d4721667f9d082fa703
Jack Kavanagh <jackkav@gmail.com>
2017-03-09 00:13:51 +0800

    added lint and fixed linter errors

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,11 @@
 import * as Koa from 'koa'
 import * as Router from 'koa-router'
-const convert = require('koa-convert')
-const graphqlHTTP = require('koa-graphql')
-import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLList
-} from 'graphql'
-const GraphQLDate = require('graphql-date')
-import { MongoClient } from 'mongodb'
+import convert = require('koa-convert')
+import graphqlHTTP = require('koa-graphql')
+import GraphQLDate = require('graphql-date')
 import * as fs from 'fs'
 import { makeExecutableSchema } from 'graphql-tools'
-
+import { MongoClient } from 'mongodb'
 
 const app = new Koa()
 const router = new Router()
@@ -26,90 +18,37 @@ const resolverMap = {
       const startDate = args.startDateInSeconds
       const endDate = args.endDateInSeconds
 
-      const appointmentObjects = await db.collection('appointments').find({
-        // appointmentTime: {
-        //   $gte: startDate,
-        //   $lt: endDate,
-        // }
+      const appointmentObjects = await db.collection('appointments').find({// all
       }).toArray()
 
       // console.log(appointmentObjects)
-      const convertedAppointments = appointmentObjects.map((a: any) => ({ date: a.appointmentTime, nickname: a.nickname }))
+      const convertedAppointments = appointmentObjects.map(
+        (a: any) => ({ date: a.appointmentTime, nickname: a.nickname }),
+        )
 
       // console.log(convertedAppointments)
       return convertedAppointments
 
-    }
-  }
+    },
+  },
 }
 
-// const AppointmentType = new GraphQLObjectType({
-//   name: 'Appointment',
-//   fields: {
-//     date: { type: GraphQLDate }
-//   }
-// })
-
-// const queryType = new GraphQLObjectType({
-//   name: 'RootQuery',
-//   fields: {
-//     appointments: {
-//       type: new GraphQLList(AppointmentType),
-//       description: 'All appointments on a given date',
-//       args: {
-//         startDateInSeconds: {
-//           type: GraphQLDate,
-//           description: 'The inclusive lower bound for the appointment date.',
-//           // defaultValue: 0,
-//         },
-//         endDateInSeconds: {
-//           type: GraphQLDate,
-//           description: 'The exclusive upper bound for the appointment date.',
-//           // defaultValue: new Date('1/1/2039').getTime(),
-//         },
-//       },
-//       resolve: async (_, args, { db }) => {
-//         const startDate = args.startDateInSeconds
-//         const endDate = args.endDateInSeconds
-
-//         const appointmentObjects = await db.collection('appointments').find({
-//           appointmentTime: {
-//             $gte: startDate,
-//             $lt: endDate,
-//           }
-//         }).toArray()
-
-//         // console.log(appointmentObjects)
-//         const convertedAppointments = appointmentObjects.map((a: any) => ({ date: a.appointmentTime, nickname: a.nickname }))
-
-//         // console.log(convertedAppointments)
-//         return convertedAppointments
-
-//         // return appointments
-//       }
-//     }
-//   }
-// })
-
-// const schema = new GraphQLSchema({
-//   query: queryType,
-// })
-
 const schema = makeExecutableSchema({
-  typeDefs: schemasText,
   resolvers: resolverMap,
+  typeDefs: schemasText,
 })
 
-MongoClient.connect('mongodb://paperKingDevelopingByiHealth:d3Wrg40dE@120.131.8.26:27017/paper-king-developing').then(db => {
-  console.log('Connected')
+MongoClient.connect('mongodb://paperKingDevelopingByiHealth:d3Wrg40dE@120.131.8.26:27017/paper-king-developing')
+.then((db) => {
+  // console.log('Connected')
 
   router.all('/graphql', convert(graphqlHTTP({
+    context: { db },
     schema,
     graphiql: true,
-    context: { db },
   })))
 
-  app.use(router.routes()).use(router.allowedMethods());
+  app.use(router.routes()).use(router.allowedMethods())
 
   app.listen(3080)
 })
