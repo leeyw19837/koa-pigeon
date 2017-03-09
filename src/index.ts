eed747a0fe3b723819ba0226690065320f1a3836
Jack Kavanagh <jackkav@gmail.com>
2017-03-09 13:33:37 +0800

    skin and medical history parsed

--- src/index.ts
+++ src/index.ts
@@ -55,8 +55,14 @@ const resolverMap = {
               running: undefinedFalse(a, 'footgear.question16.running'),
               hiking: undefinedFalse(a, 'footgear.question16.hiking'),
             },
+            medicalHistory: {
+              historyPresent: undefinedFalse(a, 'medicalHistory.had'),
+              history: historyParser(a, 'medicalHistory') ,
+            },
             skin: {
-
+              abnormalityPresent: undefinedFalse(a, 'skinConditions.had'),
+              abnormalities: skinParser(a, 'skinConditions'),
+              footTemperatures: temperatureParser(a, 'skinConditions'),
             },
             bone: {
               deformitiesLeftPresent: undefinedFalse(a, 'boneAndJoint.deformityLeft.had'),
@@ -123,39 +129,69 @@ const pulseParser = (object: object, path: string) => {
 }
 const deformityParser = (object: object, path: string) => {
   const value = get (object, path, false)
-  const symptoms = {
+  return {
     bunion: get(object, path + '.discoloration', false),
     charcotFoot: get(object, path + '.edema', false),
     hammertoe: get(object, path + '.bladder', false),
     clawToe: get(object, path + '.cracks', false),
     malletToe: get(object, path + '.callus', false),
   }
-  return symptoms
+}
+const historyParser = (object: object, path: string) => {
+  const value = get (object, path, false)
+  return {
+    hadFootUlcer: get(object, path + '.footUlcer', false),
+    amputee: get(object, path + '.amputation', false),
+    recievedFootcareInstruction: get(object, path + '.education', false),
+    livesAlone: get(object, path + '.liveAlone', false),
+  }
+}
+const skinParser = (object: object, path: string) => {
+  const value = get (object, path, false)
+  return {
+    discoloration: get(object, path + '.discoloration', false),
+    edema: get(object, path + '.edema', false),
+    flakySkin: get(object, path + '.desquamation', false),
+    blister: get(object, path + '.bladder', false),
+    cracks: get(object, path + '.cracks', false),
+    callus: get(object, path + '.callus', false),
+    ingrownToenail: get(object, path + '.qianjian', false),
+    athletesFoot: get(object, path + '.zuxuan', false),
+    toenailFungus: get(object, path + '.jiajian', false),
+  }
+}
+const temperatureParser = (object: object, path: string) => {
+  const value = get (object, path, false)
+  const unit = 'C'
+  return {
+    environment: {value: get(object, path + '.temperatureOfEnvironment', false), unit},
+    rightMetatarsal: {value: get(object, path + '.temperatureOfRightMetatarsal', false), unit},
+    leftMetatarsal: {value: get(object, path + '.temperatureOfLeftMetatarsal', false), unit},
+    right: {value: get(object, path + '.temperatureOfRight', false), unit},
+    left: {value: get(object, path + '.temperatureOfLeft', false), unit},
+  }
 }
 const limitationParser = (object: object, path: string) => {
   const value = get (object, path, false)
-  const symptoms = {
+  return {
     backStretch: get(object, path + '.BackStretchLimited', false),
     plantarFlexion: get(object, path + '.PlantarFlexionLimited', false),
   }
-  return symptoms
 }
 const bloodSymptomParser = (object: object, path: string) => {
   const value = get (object, path, false)
-  const symptoms = {
+  return {
     restPain: get(object, path + '.restPain', false),
     intermittentClaudication: get(object, path + '.intermittentClaudication', false),
   }
-  return symptoms
 }
 const nerveSymptomParser = (object: object, path: string) => {
   const value = get (object, path, [])
-  const symptoms = {
+  return {
     pain: value.includes('疼痛'),
     numbness: value.includes('麻木'),
     paresthesia: value.includes('感觉异常'),
   }
-  return symptoms
 }
 // NOTE: this will return a false value for instances where the actual value is missing, undefined or empty string
 const undefinedFalse = (object: object, path: string) => {
