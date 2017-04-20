3af2f368613dc4e9cd581d6a01226822b8f1322a
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-04-20 12:49:09 +0800

    Update photo query

--- src/mutations/photos.ts
+++ src/mutations/photos.ts
@@ -3,23 +3,16 @@ import { uploadBase64Img } from '../utils/ks3'
 
 export default {
   savePhoto: async (_, args, { db }) => {
-    const { patientId, data, context, notes } = args
+    const { patientId, data, owner, note } = args
 
     const photoUrlKey = `${patientId}${Date.now()}`
     const url = await uploadBase64Img(photoUrlKey, data)
 
-    const oldContext = (() => {
-      switch (context) {
-        case 'FOOT_ASSESSMENT': return 'footAssessment'
-        default: throw new TypeError(`Unknown context ${context}`)
-      }
-    })()
-
     const photo = {
       patientId,
       url,
-      owner: oldContext || '',
-      note: notes || '',
+      owner,
+      note,
       createdAt: new Date(),
     }
 
