f013c9cf0600a8ec2935c634470c510161985f1c
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-05-22 17:13:09 +0800

    :fire: Remove wutong's session management code

--- src/mutations/index.ts
+++ src/mutations/index.ts
@@ -1,11 +1,9 @@
 import assessmentTimes from './assessmentTimes'
 import footAssessment from './footAssessment'
 import photos from './photos'
-import signingInAndOut from './signingInAndOut'
 
 export default {
   ...footAssessment,
   ...photos,
-  ...signingInAndOut,
   ...assessmentTimes,
 }
