08fc71b050abc5281ab9abfd05a870b196033166
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-06-07 18:50:00 +0800

    Add timestamp to chat messages

--- src/queries/chatMessages.ts
+++ src/queries/chatMessages.ts
@@ -1,8 +1,15 @@
 import { Db } from 'mongodb'
 
 
-export const chatMessages = async (_, args, { db }: { db: Db }) => db
-  .collection('chatMessages')
-  .find({ appointmentId: args.appointmentId })
-  .sort({ sentAt: 1 })
-  .toArray()
+export const chatMessages = async (_, args, { db }: { db: Db }) => {
+  const messages = await db
+    .collection('chatMessages')
+    .find({ appointmentId: args.appointmentId })
+    .sort({ sentAt: 1 })
+    .toArray()
+
+  return {
+    messages,
+    timestamp: new Date().toISOString(),
+  }
+}
