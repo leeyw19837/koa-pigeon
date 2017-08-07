9da146e2f1450a130e48740130d9e19fa6b9c645
Jack Kavanagh <jackkav@gmail.com>
2017-08-07 15:38:15 +0800

    can parse all but sportsnote field

--- src/queries/bloodGlucoseMeasurements.ts
+++ src/queries/bloodGlucoseMeasurements.ts
@@ -1,20 +1,49 @@
 import { IContext } from '../types'
 
-export const bloodGlucoseMeasurements = async (_, args, { getDb }: IContext) => {
+export const bloodGlucoseMeasurements = async (
+  _,
+  args,
+  { getDb }: IContext,
+) => {
   const db = await getDb()
-  const a = await db.collection('bloodglucoses').find({pillNote: {$exists: 1, $ne: ''} {limit: 50}).toArray()
-  return a.map(x => ({...x,
-     measuredAt: x.createdAt,
-     patient: {_id: x.author},
-     relationshipToMealTime: DigestiveStateLookup[x.dinnerSituation],
-     bloodGlucose: {value: (+x.bgValue).toFixed(2), unit: 'mmol/L'},
-     manual: !!x.source,
-     medication: structureMedication(x.pillNote),
-    }))
+  const a = await db
+    .collection('bloodglucoses')
+    .find({}, { limit: 50 })
+    .toArray()
+  return a.map(x => ({
+    ...x,
+    measuredAt: x.createdAt,
+    patient: { _id: x.author },
+    digestiveState: DigestiveStateLookup[x.dinnerSituation],
+    bloodGlucose: { value: (+x.bgValue).toFixed(2), unit: 'mmol/L' },
+    manual: !!x.source,
+    medication: structureMedication(x.pillNote),
+    appName: !!x.iGlucoseDataId && 'IGLUCO',
+    measurementDeviceModel: !!x.iGlucoseDataId && 'BG1',
+    carbohydratesConsumed: structureCarbohydrates(x.mealNote),
+    hadTakenInsulin: ChineseBoolean[x.insulinInjection],
+  }))
+}
+const structureCarbohydrates = mealNote => {
+  if (!mealNote) return null
+  return {
+    unit: mealNote.match(/[a-zA-Z]+|[0-9]+/g)[1],
+    value: parseFloat(mealNote),
+  }
 }
 const structureMedication = pillNote => {
   if (!pillNote) return
-  return pillNote.map(y => ({type: y.type, unit: y.value.match(/[a-zA-Z]+|[0-9]+/g)[1], value: parseFloat(y.value)}))
+  return pillNote
+    .map(y => ({
+      type: y.type,
+      unit: y.value.match(/[a-zA-Z]+|[0-9]+/g)[1],
+      value: parseFloat(y.value),
+    }))
+    .filter(z => z.value !== 0)
+}
+const ChineseBoolean = {
+  是: true,
+  否: false,
 }
 const DigestiveStateLookup = {
   空腹: 'EMPTY_STOMACH',
