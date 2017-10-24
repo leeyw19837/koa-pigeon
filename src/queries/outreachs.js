0ed74cfa8d3b65f306dd5d1537fa9da37af4c373
QiLin <edisonqlljl@hotmail.com>
2017-10-24 22:29:55 +0800

    updated code in order to compatible all query of outreachs

--- src/queries/outreachs.js
+++ src/queries/outreachs.js
@@ -2,23 +2,42 @@ const moment = require('moment')
 
 export const outreachs = async (_, args, { getDb }) => {
   const db = await getDb()
-  const { period } = args
+  const { period, startDay = new Date('1949/10/1') } = args
   let before = moment().startOf('day')._d
-  switch (period) {
-    case 'all':
-      before = moment().subtract(50, 'years').startOf('day')._d
-      break
-    case '3day':
-      before = moment().subtract(3, 'days').startOf('day')._d
-      break
-    case '7day':
-      before = moment().subtract(7, 'days').startOf('day')._d
-      break
-    default:
-      before = moment().startOf('day')._d
+  let query = {
+    isHandle: false,
+    appointmentTime: { $gt: moment(startDay).startOf('day')._d }
   }
 
-  return db.collection('outreachs').find({
-    appointmentTime: { $gt: before }
-  }).toArray()
+  switch (period) {
+    case 'oneDay':
+      const startOfCurrentWeek = moment(startDay).startOf('isoWeek')
+      const endOfCurrentWeek = moment(startOfCurrentWeek._d).add(7, 'd')
+      query = {
+        isHandle: false,
+        $and: [
+          { appointmentTime: { $gt: startOfCurrentWeek._d } },
+          { appointmentTime: { $lt: endOfCurrentWeek._d } }
+        ]
+      }
+      break
+    case 'oneWeek':
+      const startOfCurrentWeek = moment(startDay).startOf('isoWeek')
+      const endOfCurrentWeek = moment(startOfCurrentWeek._d).add(7, 'd')
+      query = {
+        isHandle: false,
+        $and: [
+          { appointmentTime: { $gt: startOfCurrentWeek._d } },
+          { appointmentTime: { $lt: endOfCurrentWeek._d } }
+        ]
+      }
+      break;
+    case 'oneMonth':
+
+      break
+    default:
+
+  }
+
+  return db.collection('outreachs').find(query).toArray()
 }
--- src/resolvers/outreachs.js
+++ src/resolvers/outreachs.js
@@ -3,7 +3,6 @@ import { ObjectID } from 'mongodb'
 export const Outreach = {
   patient: async (outreach, _, { getDb }) => {
     const db = await getDb()
-    console.log(outreach.patientId)
     return db.collection('users').findOne({
       _id: ObjectID.createFromHexString(outreach.patientId)
     })
