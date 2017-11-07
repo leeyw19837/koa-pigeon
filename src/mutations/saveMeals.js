a48cdbb0d0bbeaaa7c670457ba5255935be324dc
tangweikun <819573105@qq.com>
2017-11-07 21:52:50 +0800

    fetchDiets

--- src/mutations/saveMeals.js
+++ src/mutations/saveMeals.js
@@ -2,7 +2,7 @@ export const saveMeals = async (_, args, context) => {
   const db = await context.getDb()
   const { patientId, mealTime, food } = args
 
-  await db.collection('mealHistorys').insertOne({
+  await db.collection('mealHistories').insertOne({
     patientId,
     mealTime,
     food,
