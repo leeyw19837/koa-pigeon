188ae12d4202edcdf1e8495b9c0b574625500556
蒋凯 <baidan4855@qq.com>
2017-11-15 16:11:21 +0800

    professionalLogin with username

--- src/queries/healthCareProfessional.ts
+++ src/queries/healthCareProfessional.ts
@@ -4,14 +4,14 @@ export const professionalLogin = async (_, args, { getDb }: IContext) => {
   const db = await getDb()
 
   // const clientCodename = context.state.clientCodename
-  const { mobile, password } = args
+  const { username, password } = args
   if(password !== 'zhufanglu2017') {
     throw new Error('密码错误！')
   }
 
   const existingDoctor = await db
     .collection('users')
-    .findOne({ mobile })
+    .findOne({ username })
   if (!existingDoctor) {
     throw new Error('用户不存在！')
   }
