500855ee44619045187531db61f6c9ba1c582cf5
Jack Kavanagh <jackkav@gmail.com>
2017-11-17 14:55:07 +0800

    removed status filter in closest appointment

--- src/resolvers/patient.ts
+++ src/resolvers/patient.ts
@@ -4,7 +4,6 @@ const moment = require('moment')
 export const Patient = {
   footAssessmentPhotos: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
-
     return db
       .collection('photos')
       .find({
@@ -15,45 +14,43 @@ export const Patient = {
   },
   needleChatRoom: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
-
-    return db
-      .collection('needleChatRooms')
-      .findOne({
-        _id: patient.needleChatRoomId,
-      })
+    return db.collection('needleChatRooms').findOne({
+      _id: patient.needleChatRoomId,
+    })
   },
-  closestAppointment: async(patient, _, { getDb }: IContext) => {
+  closestAppointment: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
     const endOfToday = moment().endOf('day')._d
-    const result = await db.collection('appointments').find({
-      patientId: patient._id.toString(),
-      status: 0,
-      appointmentTime: {
-        $gt: endOfToday,
-      },
-    }).sort({appointmentTime: 1}).toArray()
+    const result = await db
+      .collection('appointments')
+      .find({
+        patientId: patient._id.toString(),
+        appointmentTime: {
+          $gt: endOfToday,
+        },
+      })
+      .sort({ appointmentTime: 1 })
+      .toArray()
     return result[0] || null
   },
-  communications: async(patient, _, { getDb }: IContext) => {
+  communications: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
-    
     return db
       .collection('communication')
       .find({
         patientId: patient._id.toString(),
       })
-      .sort({ createdAt: -1})
+      .sort({ createdAt: -1 })
       .toArray()
   },
-  caseRecords: async(patient, _, { getDb }: IContext) => {
+  caseRecords: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
-
     return db
       .collection('caseRecord')
       .find({
         patientId: patient._id.toString(),
       })
-      .sort({ createdAt: -1})
+      .sort({ createdAt: -1 })
       .toArray()
-  }
+  },
 }
