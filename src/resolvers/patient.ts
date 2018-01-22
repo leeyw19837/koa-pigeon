5ff16f0b9d49564917d724b0f651bf1a49a2a40b
dyk <dyk900502@163.com>
2018-01-22 17:44:35 +0800

    fix a bug

--- src/resolvers/patient.ts
+++ src/resolvers/patient.ts
@@ -1,5 +1,6 @@
 import get = require('lodash/get')
 import { IContext } from '../types'
+import { maybeCreateFromHexString } from '../utils/maybeCreateFromHexString'
 
 const moment = require('moment')
 
@@ -65,7 +66,7 @@ export const Patient = {
       .sort({ createdAt: -1 })
       .toArray()
   },
-  outHospitalSoaps: async (patient, _, {getDb}: IContext) => {
+  outHospitalSoaps: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
     return db
       .collection('outHospitalSoap')
@@ -79,11 +80,11 @@ export const Patient = {
     const db = await getDb()
     if (patient.healthCareTeamId && patient.healthCareTeamId.length > 0) {
       return db
-      .collection('healthCareTeams')
-      .find({
-        _id: patient.healthCareTeamId[0],
-      })
-      .toArray()
+        .collection('healthCareTeams')
+        .find({
+          _id: patient.healthCareTeamId[0],
+        })
+        .toArray()
     }
     return []
   },
@@ -101,51 +102,52 @@ export const Patient = {
     const db = await getDb()
     const result = await db
       .collection('appointments')
-        .find({
-          patientId: patient._id.toString(),
-        })
-        .sort({ appointmentTime: -1 })
-        .toArray()
+      .find({
+        patientId: patient._id.toString(),
+      })
+      .sort({ appointmentTime: -1 })
+      .toArray()
     return result[0] || null
   },
   useIGluco: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
     const result = await db
       .collection('users')
-        .find({
-          _id: patient._id.toString(),
-        })
-        .toArray()
+      .find({
+        _id: maybeCreateFromHexString(patient._id),
+      })
+      .toArray()
+    console.log('==========useIGluco/result=======', result)
     return !!get(result[0], 'iGlucoseUserId')
   },
   useNeedle: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
     const result = await db
       .collection('users')
-        .find({
-          _id: patient._id.toString(),
-        })
-        .toArray()
+      .find({
+        _id: maybeCreateFromHexString(patient._id),
+      })
+      .toArray()
     return !!get(result[0], 'isUseNeedle')
   },
   useSPT: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
     const result = await db
       .collection('users')
-        .find({
-          _id: patient._id.toString(),
-        })
-        .toArray()
+      .find({
+        _id: maybeCreateFromHexString(patient._id),
+      })
+      .toArray()
     return !!get(result[0], 'deviceSPT')
   },
   usePublicNumber: async (patient, _, { getDb }: IContext) => {
     const db = await getDb()
     const result = await db
       .collection('users')
-        .find({
-          _id: patient._id.toString(),
-        })
-        .toArray()
+      .find({
+        _id: maybeCreateFromHexString(patient._id),
+      })
+      .toArray()
     return !!get(result[0], 'wechatTag')
   },
 }
