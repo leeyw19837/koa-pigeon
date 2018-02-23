2b62fc06da4c362e6d8a08d5893a5cb58e512eb3
dyk <dyk900502@163.com>
2018-02-23 15:01:03 +0800

    fix a bug

--- src/mutations/addOrder.js
+++ src/mutations/addOrder.js
@@ -1,7 +1,7 @@
 import freshId from 'fresh-id'
 const moment = require('moment')
 
-export const addOutHospitalSoap = async (_, args, context) => {
+export const addOrder = async (_, args, context) => {
   const db = await context.getDb()
   const {
     patientId,
@@ -34,9 +34,7 @@ export const addOutHospitalSoap = async (_, args, context) => {
     totalPrice,
   })
   if (!!result.result.ok) {
-    console.log('保存成功了哈哈哈哈哈-------')
     return true
   }
-  console.log('保存失败了哈哈哈哈哈-------')
   return false
 }
