3eab37f9847f5e5e1036a3034a83f9f66dda5257
dyk <dyk900502@163.com>
2018-04-10 15:24:57 +0800

    fix a bug

--- src/queries/bloodMeasurementPlans.js
+++ src/queries/bloodMeasurementPlans.js
@@ -64,7 +64,7 @@ export const bloodMeasurementPlans = async (_, args, { getDb }) => {
         $lte: dateofweek.format('YYYY-MM-DD'),
       },
       endAt: {
-        $gte: dateofweek.format('YYYY-MM-DD'),
+        $gte: endofweek.format('YYYY-MM-DD'),
       },
     })
     .toArray()
