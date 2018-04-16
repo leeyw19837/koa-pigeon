58c3b6d504c8c390014cc426b1e4bd2e80701983
leeyw19837 <leeyw19837@hotmail.com>
2018-04-16 13:29:45 +0800

    new schema for needle: compatible with old apps.

--- src/queries/bloodGlucoseMeasurementsAndTreatmentPlans.ts
+++ src/queries/bloodGlucoseMeasurementsAndTreatmentPlans.ts
@@ -1,109 +1,127 @@
-import { ObjectId } from 'mongodb'
 import { IContext } from '../types'
+import { DigestiveStateLookup } from '../utils/i18n'
+
+import { ObjectId } from 'mongodb'
 
 export const bloodGlucoseMeasurementsAndTreatmentPlans = async (
-  _,
-  args,
-  { getDb }: IContext,
+    _,
+    args,
+    { getDb }: IContext,
 ) => {
-  const db = await getDb()
-  // const bloodGlucoseMeasurementsResult = await db
-  //   .collection('bloodglucoses')
-  //   .find(cursor)
-  //   .sort({ createdAt: -1 })
-  //   .toArray()
+    const db = await getDb()
+    const cursor = { author: args.patientId }
+    if (args.from && args.to) {
+        Object.assign(cursor, {
+            createdAt: { $gt: args.from, $lt: args.to },
+        })
+    }
 
-  const query = {
-    patientId: args.patientId,
-    measuredAt: { $gt: args.from, $lt: args.to },
-    dataStatus: { $ne: 'DELETED' },
-  }
+    const bloodGlucoseMeasurementsResult = await db
+        .collection('bloodglucoses')
+        .find(cursor)
+        .sort({ createdAt: -1 })
+        .toArray()
 
-  const bloodGlucoseMeasurementsResult = await db
-    .collection('bloodGlucoses')
-    .find(query)
-    .sort({ measuredAt: -1 })
-    .toArray()
+    const treatmentPlansResult = await db
+        .collection('bg_measure_module')
+        .find({ patientId: args.patientId })
+        .sort({ createdAt: -1 })
+        .toArray()
 
-  const treatmentPlansResult = await db
-    .collection('bg_measure_module')
-    .find({ patientId: args.patientId })
-    .sort({ createdAt: -1 })
-    .toArray()
+    const treatmentPlans = treatmentPlansResult.map(item => ({
+        startAt: item.startAt,
+        endAt: item.endAt,
+        testTimes: {
+            monday: convertObjectToArray(item.Monday),
+            tuesday: convertObjectToArray(item.Tuesday),
+            wednesday: convertObjectToArray(item.Wednesday),
+            thursday: convertObjectToArray(item.Thursday),
+            friday: convertObjectToArray(item.Friday),
+            saturday: convertObjectToArray(item.Saturday),
+            sunday: convertObjectToArray(item.Sunday),
+        },
+    }))
 
-  const treatmentPlans = treatmentPlansResult.map(item => ({
-    startAt: item.startAt,
-    endAt: item.endAt,
-    testTimes: {
-      monday: convertObjectToArray(item.Monday),
-      tuesday: convertObjectToArray(item.Tuesday),
-      wednesday: convertObjectToArray(item.Wednesday),
-      thursday: convertObjectToArray(item.Thursday),
-      friday: convertObjectToArray(item.Friday),
-      saturday: convertObjectToArray(item.Saturday),
-      sunday: convertObjectToArray(item.Sunday),
-    },
-  }))
-
-  const DigestiveStateMap = {
-    BEFORE_BREAKFAST: 'BEFORE_BREAKFAST',
-    AFTER_BREAKFAST: 'AFTER_BREAKFAST',
-    BEFORE_LUNCH: 'BEFORE_LUNCH',
-    AFTER_LUNCH: 'AFTER_LUNCH',
-    BEFORE_DINNER: 'BEFORE_DINNER',
-    AFTER_DINNER: 'AFTER_DINNER',
-    BEFORE_SLEEPING: 'BEFORE_SLEEP',
-    MIDNIGHT: 'MIDNIGHT',
-    RANDOM: 'UNKNOWN',
-  }
-
-  const bloodGlucoseMeasurements = bloodGlucoseMeasurementsResult.map(x => ({
-    ...x,
-    measuredAt: x.measuredAt,
-    patient: { _id: x.patientId },
-    digestiveState: DigestiveStateMap[x.measurementTime],
-    bloodGlucose: {
-      value: (+x.bloodGlucoseValue).toFixed(2),
-      unit: 'mg/dL',
-    },
-    manual: x.inputType === 'MANUAL',
-    measurementDeviceModel: !!x.sourceId && 'BG1',
-    measureResultId: ObjectId.createFromHexString(x._id),
-    remark: x.note ? x.note : '',
-    // 以下三个字段，暂时无用
-    medication: [],
-    carbohydratesConsumed: 'nope',
-    hadTakenInsulin: 'nope',
-  }))
-  return { bloodGlucoseMeasurements, treatmentPlans }
+    const bloodGlucoseMeasurements = bloodGlucoseMeasurementsResult.map(x => ({
+        ...x,
+        measuredAt: x.createdAt,
+        patient: { _id: x.author },
+        digestiveState: DigestiveStateLookup[x.dinnerSituation],
+        bloodGlucose: {
+            value: (+x.bgValue).toFixed(2),
+            unit: 'mg/dL',
+        },
+        manual: !!(x.measurementDeviceModel != 'BG1'),
+        medication: structureMedication(x.pillNote),
+        measurementDeviceModel: !!x.iGlucoseDataId && 'BG1',
+        carbohydratesConsumed: structureCarbohydrates(x.mealNote),
+        hadTakenInsulin: ChineseBoolean[x.insulinInjection],
+        measureResultId: x._id,
+        remark: x.remark ? x.remark : '',
+    }))
+    return { bloodGlucoseMeasurements, treatmentPlans }
 }
 
-// const structureCarbohydrates = mealNote => {
-//     if (!mealNote) return null
-//     return {
-//         unit: mealNote.match(/[a-zA-Z]+|[0-9]+/g)[1],
-//         value: parseFloat(mealNote),
-//     }
-// }
-//
-// const structureMedication = pillNote => {
-//     if (!pillNote) return
-//     return pillNote
-//         .map(y => ({
-//             type: y.type,
-//             unit: y.value.match(/[a-zA-Z]+|[0-9]+/g)[1],
-//             value: parseFloat(y.value),
-//         }))
-//         .filter(z => z.value !== 0)
-// }
-//
-// const ChineseBoolean = {
-//     是: true,
-//     否: false,
-// }
+const structureCarbohydrates = mealNote => {
+    if (!mealNote) return null
+    return {
+        unit: mealNote.match(/[a-zA-Z]+|[0-9]+/g)[1],
+        value: parseFloat(mealNote),
+    }
+}
+
+const structureMedication = pillNote => {
+    if (!pillNote) return
+    return pillNote
+        .map(y => ({
+            type: y.type,
+            unit: y.value.match(/[a-zA-Z]+|[0-9]+/g)[1],
+            value: parseFloat(y.value),
+        }))
+        .filter(z => z.value !== 0)
+}
+
+const ChineseBoolean = {
+    是: true,
+    否: false,
+}
 
 const convertCamelCaseToUpperCase = str =>
-  str.replace(/[A-Z]/, item => `_${item}`).toUpperCase()
+    str.replace(/[A-Z]/, item => `_${item}`).toUpperCase()
 
 const convertObjectToArray = obj =>
-  Object.keys(obj).map(value => convertCamelCaseToUpperCase(value))
+    Object.keys(obj).map(value => convertCamelCaseToUpperCase(value))
+
+
+/*****************/
+
+export const bloodGlucoses = async (
+    _,
+    args,
+    { getDb }: IContext,
+) => {
+    const db = await getDb()
+
+    const query = {
+        patientId: args.patientId,
+        dataStatus: { $ne: 'DELETED' },
+    }
+
+    const bloodGlucoseMeasurementsResult = await db
+        .collection('bloodGlucoses')
+        .find(query)
+        .sort({ measuredAt: -1 })
+        .toArray()
+
+    const bloodGlucoseMeasurements = bloodGlucoseMeasurementsResult.map(x => ({
+        ...x,
+        _id: x._id,
+        bloodGlucoseValue: (+x.bloodGlucoseValue).toFixed(2),
+        bloodGlucoseDataSource: x.bloodGlucoseDataSource,
+        inputType: x.inputType,
+        measurementTime: x.measurementTime,
+        measuredAt: x.measuredAt,
+        note: x.note,
+    }))
+    return { bloodGlucoseMeasurements}
+}
