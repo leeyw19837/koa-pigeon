750b338a2312b1b25f3ccb5ab83738412b5d3861
Jack Kavanagh <jackkav@gmail.com>
2018-04-17 16:54:11 +0800

    fixed bgValue -> bloodGlucoseValue naming change

--- src/queries/warningsOfLow.js
+++ src/queries/warningsOfLow.js
@@ -5,43 +5,48 @@ export const warningsOfLow = async (_, args, { getDb }) => {
   const { period } = args
   let query = {
     warningType: 'LOW_GLUCOSE',
-    createdAt: { $gte: moment().startOf('day')._d},
-    $or : [
-      {isHandle: { $exists: false }},
-      {isHandle: false},
-    ],
+    createdAt: { $gte: moment().startOf('day')._d },
+    $or: [{ isHandle: { $exists: false } }, { isHandle: false }],
   }
   switch (period) {
     case 'threeDays':
       query = {
         warningType: 'LOW_GLUCOSE',
-        createdAt: { $gte: moment().subtract(2, 'd').startOf('day')._d },
-        $or : [
-          {isHandle: { $exists: false }},
-          {isHandle: false},
-        ],
+        createdAt: {
+          $gte: moment()
+            .subtract(2, 'd')
+            .startOf('day')._d,
+        },
+        $or: [{ isHandle: { $exists: false } }, { isHandle: false }],
       }
       break
     case 'sevenDays':
-    query = {
-      warningType: 'LOW_GLUCOSE',
-      createdAt: { $gte: moment().subtract(6, 'd').startOf('day')._d },
-      $or : [
-        {isHandle: { $exists: false }},
-        {isHandle: false},
-      ],
-    }
-      break;
+      query = {
+        warningType: 'LOW_GLUCOSE',
+        createdAt: {
+          $gte: moment()
+            .subtract(6, 'd')
+            .startOf('day')._d,
+        },
+        $or: [{ isHandle: { $exists: false } }, { isHandle: false }],
+      }
+      break
     case 'all':
       query = {
         warningType: 'LOW_GLUCOSE',
-        $or : [
-          {isHandle: { $exists: false }},
-          {isHandle: false},
-        ],
+        $or: [{ isHandle: { $exists: false } }, { isHandle: false }],
       }
       break
     default:
   }
-  return db.collection('warnings').find(query).sort({ createdAt: -1 }).toArray()
+  const warnings = await db
+    .collection('warnings')
+    .find(query)
+    .sort({ createdAt: -1 })
+    .toArray()
+  //NOTE: migrating old data to newly named field
+  return warnings.map(x => ({
+    ...x,
+    bloodGlucoseValue: x.bloodGlucoseValue || +x.bgValue,
+  }))
 }
