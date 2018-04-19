4b208c41f23060b11875266b696090da794b21e9
leeyw19837 <leeyw19837@hotmail.com>
2018-04-19 16:19:38 +0800

    fix bug: modify user device-info-query logic for mi-push service

--- src/mipush/sender.ts
+++ src/mipush/sender.ts
@@ -1,36 +1,62 @@
+import {get} from 'lodash'
+import {ObjectId} from 'mongodb'
+import {ALIAS_URL, APP_SECRET, PACKAGE_NAME, TYPE_MAP} from './constants'
 
-import { APP_SECRET, PACKAGE_NAME, ALIAS_URL, TYPE_MAP } from './constants'
-import { get } from 'lodash'
 const request = require('request-promise')
 
-export const sender = async ({ type = 'CHAT', pushId, description, db, title = '护血糖' }) =>{
+export const sender = async ({type = 'CHAT', pushId, description, db, title = '护血糖'}) => {
 
-    const arr = await db.collection('bloodglucoses').find({author: pushId})
-        .sort({createdAt: -1}).limit(1).toArray()
-    const data = arr[0]
-    console.log(data, 'mipush=00=======')
-    if(data && data.deviceContext.systemName){
-        const systemName = get(data,'deviceContext.systemName','android').toLowerCase()
-        realSender({type, pushId, description, db, title,systemName})
-    } else{
-        realSender({type, pushId, description, db, title,systemName:'android'})
-        realSender({type, pushId, description, db, title,systemName:'ios'})
+    const userDeviceContextFromUserCollection = await db.collection('users')
+        .find({_id: ObjectId.createFromHexString(pushId)})
+        .sort({updatedAt: -1})
+        .limit(1)
+        .toArray()
+
+    const dataFromUserCollection = userDeviceContextFromUserCollection[0]
+
+    if (dataFromUserCollection && dataFromUserCollection.deviceContext.systemName) {
+        const systemName = get(dataFromUserCollection, 'deviceContext.systemName', 'android').toLowerCase()
+        realSender({type, pushId, description, db, title, systemName})
+    } else {
+        const userDeviceContextFromBGCollection = await db.collection('bloodGlucoses')
+            .find({patientId: pushId})
+            .sort({updatedAt: -1})
+            .limit(1)
+            .toArray()
+
+        const dataFromBGCollection = userDeviceContextFromBGCollection[0]
+
+        console.log('MIPush 异常：未在 users 表中查询到该 patientId 对应的设备信息，查询 bloodGlucoses 表。patientId = ', pushId)
+
+        if (dataFromBGCollection && dataFromBGCollection.deviceInformation.bundleId) {
+            const bundleId = get(dataFromBGCollection, 'deviceInformation.bundleId', 'com.ihealth.HuTang')
+            realSender({
+                type,
+                pushId,
+                description,
+                db,
+                title,
+                systemName: (bundleId === 'com.ihealth.HuTang' ? 'android' : 'ios'),
+            })
+        } else {
+            console.log('MIPush 异常：同时未在 bloodGlucoses 表中查询到测量历史，无法确定设备类型，不进行消息推送。patientId = ', pushId)
+        }
+
     }
 }
 
-const realSender = async ({ type = 'CHAT', pushId, description, db, title = '护血糖',systemName }) =>{
+const realSender = async ({type = 'CHAT', pushId, description, db, title = '护血糖', systemName}) => {
     const notifyForeGround = type === 'CHAT' ? '0' : '1'
 
-    //请求小米发出推送
     const formData = {
         alias: pushId,
         restricted_package_name: PACKAGE_NAME[systemName],
-        payload: JSON.stringify({ type }),
+        payload: JSON.stringify({type}),
         pass_through: 0,
         notify_type: -1,
         notify_id: TYPE_MAP[type] || 1,
         extra: {
-          notify_foreground: notifyForeGround,
+            notify_foreground: notifyForeGround,
         },
         title,
         description,
@@ -40,15 +66,15 @@ const realSender = async ({ type = 'CHAT', pushId, description, db, title = '护
         method: 'POST',
         uri: ALIAS_URL,
         headers: {
-          Authorization: `key=${APP_SECRET[systemName]}`,
+            Authorization: `key=${APP_SECRET[systemName]}`,
         },
         form: formData,
     }
 
-    console.log(options, 'mipush')
+    console.log('mipush options ===', options)
+
     const response = await request(options)
 
-    console.log(response, 'mipush')
-    //向db插入一条推送记录
+    console.log('mipush response ===', response)
 
 }
