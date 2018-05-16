fc2aa782e05efaaa32674c50c67bb382a600bd6f
KaroseLiu <kusoroselh@gmail.com>
2018-05-16 16:09:28 +0800

    Change the send api from array to on

--- src/cronJob/controller/sendMiniProgram.js
+++ src/cronJob/controller/sendMiniProgram.js
@@ -6,47 +6,50 @@ const moment = require('moment')
 const request = require('request-promise')
 const { NODE_ENV } = process.env
 
-const sendApi = async (patientId) => {
+const sendApi = async (patientIds) => {
   const url = 'https://wx-api-gtzh.ihealthlabs.com.cn'
-  const uri = `${url}/wechat/mini-program/send?patientId=${patientId}&pwd=4Z21FjF`
+  const uri = `${url}/wechat/mini-program/send`
   const options = {
-    method: 'GET',
+    method: 'POST',
     uri,
+    json: true,
+    headers: {
+        Authorization: '4Z21FjF',
+    },
+    body: {
+      userIds: patientIds,
+    },
   }
   return await request(options)
 }
 
 export const sendMiniProgram = async (period, patientId) => {
+  if(patientId) {
+    await sendApi([patientId])
+  }
   if (NODE_ENV === 'production') {
-    if(patientId) {
-      await sendApi(patientId)
-    } else {
-      const currentDay = moment().isoWeekday()
-      const appointments = await getAppointments()
-      const availableAppointments = appointments.filter(o => {
-        const { healthCareTeamId } = o
-        return (
-          healthCareTeamMap[healthCareTeamId]
-          && healthCareTeamMap[healthCareTeamId][currentDay]
-          && healthCareTeamMap[healthCareTeamId][currentDay] === period
-        )
-      })
+    const currentDay = moment().isoWeekday()
+    const appointments = await getAppointments()
+    const availableAppointments = appointments.filter(o => {
+      const { healthCareTeamId } = o
+      return (
+        healthCareTeamMap[healthCareTeamId]
+        && healthCareTeamMap[healthCareTeamId][currentDay]
+        && healthCareTeamMap[healthCareTeamId][currentDay] === period
+      )
+    })
 
-      const patientIds = availableAppointments.map(o => o.patientId)
-      const patients = await getPatientsByIds(patientIds)
-      const hasWechatInfo = patients.filter(o => o.wechatInfo && !!o.wechatInfo.openid)
-      const hasWechatPids = hasWechatInfo.map(o => o._id.toString())
-      const caseRecords = await getCaseRecords(hasWechatPids)
-      const availablePids = caseRecords.map(o => o.patientId)
-      if (availablePids.length) {
-        for (let i = 0; i < availablePids.length; i++) {
-          const pId = availablePids[i]
-          try {
-            await sendApi(pId)
-          } catch (e) {
-            console.log(e)
-          }
-        }
+    const patientIds = availableAppointments.map(o => o.patientId)
+    const patients = await getPatientsByIds(patientIds)
+    const hasWechatInfo = patients.filter(o => o.wechatInfo && !!o.wechatInfo.openid)
+    const hasWechatPids = hasWechatInfo.map(o => o._id.toString())
+    const caseRecords = await getCaseRecords(hasWechatPids)
+    const availablePids = caseRecords.map(o => o.patientId)
+    if (availablePids.length) {
+      try {
+        await sendApi(availablePids)
+      } catch (e) {
+        console.log(e)
       }
     }
   }
