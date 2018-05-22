b5043fec32646590ed5dce965472737e1dc7a6f2
leeyw19837 <leeyw19837@hotmail.com>
2018-05-22 16:30:06 +0800

    fix bug: doctors' avatars interface

--- src/queries/getPatientInstitution.js
+++ src/queries/getPatientInstitution.js
@@ -1,17 +1,33 @@
 import { ObjectID } from 'mongodb'
 import { IContext } from '../types'
+import {HealthCareTeam} from "../resolvers/healthCareTeam";
+
+const healthCareTeams = {
+    healthCareTeam1:'BEIDAYIYUAN',
+    healthCareTeam2:'LUHEYIYUAN',
+    healthCareTeam3:'CHAOYANGYIYUAN',
+    healthCareTeam5:'SHOUGANGYIYUAN',
+    healthCareTeam4:'OTHERS',
+    healthCareTeam:'OTHERS',
+}
 
 export const getPatientInstitution = async (_, args, { getDb }) => {
     const db = await getDb()
     let result = {institutionName:'EMPTY'}
+    let healthcareTeamId = []
     const user = await db
         .collection('users')
         .find({_id:ObjectID.createFromHexString(args.patientId)})
         .toArray()
     if (user && user.length>0){
-        result.institutionName = user[0].institutionId
+        healthcareTeamId = user[0].healthCareTeamId
+        if (healthcareTeamId && healthcareTeamId.length>0){
+            result.institutionName = healthCareTeams[healthcareTeamId[0]]
+        }else {
+            result.institutionName = 'OTHERS'
+        }
     }else {
-        result.institutionName = 'lalalala'
+        result.institutionName = 'OTHERS'
     }
     return result
 }
