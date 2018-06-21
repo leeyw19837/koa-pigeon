90d6aeb15ca56b3eb200ad9943937b1ee1eda0fb
bibotai <bibotaixsf@gmail.com>
2018-06-21 14:22:08 +0800

    Added a function that delete delay event

--- src/redisCron/controller/index.js
+++ src/redisCron/controller/index.js
@@ -1,13 +1,14 @@
 const redis = require('redis')
-
+// redis 连接配置
 const redis_config = {
   host: 'redis',
   port: 6379,
 }
-
+// 建立两个客户端 一个负责发布一个负责订阅
 const client = redis.createClient(redis_config)
 const sub = redis.createClient(redis_config)
 
+// 处理错误信息
 client.on('error', function(err) {
   console.log('client Error ' + err)
 })
@@ -16,7 +17,9 @@ sub.on('error', function(err) {
   console.log('sub Error ' + err)
 })
 
-const eventKeyPrefix = 'custom_event_' // 任务列表
+const eventKeyPrefix = 'pigeon_' // key前置内容 用于区分自定义事件和其他事件
+
+// 添加定时任务事件 key为你要记录的内容 delay为延时执行的时间
 export const addDelayEvent = (key, delay = 10 * 60) => {
   client.setex(`${eventKeyPrefix}_${key}`, delay, 'delay event', err => {
     if (err) {
@@ -26,11 +29,23 @@ export const addDelayEvent = (key, delay = 10 * 60) => {
   })
 }
 
+// 删除定时任务事件
+export const deleteDelayEvent = (key, delay = 10 * 60) => {
+  client.del(`${eventKeyPrefix}_${key}`, err => {
+    if (err) {
+      return console.log('删除延迟事件失败：', err)
+    }
+    console.log('删除延迟事件成功')
+  })
+}
+
+// 订阅消息 当匹配到自定义事件key即为定时时间到
 sub.on('pmessage', (pattern, channel, message) => {
   // match key
   const keyMatcher = new RegExp(`^${eventKeyPrefix}_(\\S+)$`)
   const result = message.match(keyMatcher)
   if (result) {
+    // 在这里添定时任务
     console.log('订阅消息：%s', result[1])
   }
 })
--- src/redisCron/router/index.js
+++ src/redisCron/router/index.js
@@ -1,9 +1,13 @@
-import { addDelayEvent } from '../controller'
+import { addDelayEvent, deleteDelayEvent } from '../controller'
 const Router = require('koa-router')
 const redisCron = new Router()
-
+// 调用测试
 redisCron.get('/testmessage', async ctx => {
-  addDelayEvent('testmessage', 5)
+  addDelayEvent('testmessage', 10)
+})
+
+redisCron.get('/deltestmessage', async ctx => {
+  deleteDelayEvent('testmessage')
 })
 
 export default redisCron
