d675a07fe346b974a622984e74cdb9eb4bb7d8cb
KaroseLiu <kusoroselh@gmail.com>
2018-07-04 18:02:31 +0800

    Add the blog query

---
+++ src/resolvers/blogs.js
@@ -0,0 +1,28 @@
+import { ObjectID } from 'mongodb'
+
+export const Blog = {
+  comments: async (blog, _, { getDb }) => {
+    const db = await getDb()
+    const { comments } = blog
+    const transforComment = []
+    if (comments.length) {
+      const users = await db
+        .collection('users')
+        .find({
+          _id: { $in: comments.map(o => ObjectID(o.userId)) },
+        })
+        .toArray()
+      comments.forEach(comment => {
+        const { text, votes, createdAt, userId } = comment
+        const user = users.filter(o => o._id.toString() === userId)[0] || {}
+        transforComment.push({
+          text,
+          user,
+          votes,
+          createdAt,
+        })
+      })
+    }
+    return transforComment
+  },
+}
