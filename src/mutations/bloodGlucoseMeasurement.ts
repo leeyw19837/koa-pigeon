239956315cc01553eb833d4b235e40c1cd932bee
bibotai <bibotaixsf@gmail.com>
2018-07-23 14:29:55 +0800

    stop the 2 hours remind

--- src/mutations/bloodGlucoseMeasurement.ts
+++ src/mutations/bloodGlucoseMeasurement.ts
@@ -1,20 +1,12 @@
 import freshId from 'fresh-id'
-import { ObjectID } from 'mongodb'
-import { pubsub } from '../pubsub'
-import { IContext } from '../types'
+import {ObjectID} from 'mongodb'
+import {pubsub} from '../pubsub'
+import {IContext} from '../types'
 import * as moment from 'moment'
-import { DigestiveStateLookup } from '../utils/i18n'
-import { maybeCreateFromHexString } from '../utils/maybeCreateFromHexString'
-import {
-  addDelayEvent,
-  deleteDelayEvent,
-  queryDelayEvent,
-} from '../redisCron/controller'
-export const saveBloodGlucoseMeasurement = async (
-  _,
-  args,
-  { getDb }: IContext,
-) => {
+import {DigestiveStateLookup} from '../utils/i18n'
+import {maybeCreateFromHexString} from '../utils/maybeCreateFromHexString'
+import {addDelayEvent, deleteDelayEvent, queryDelayEvent} from '../redisCron/controller'
+export const saveBloodGlucoseMeasurement = async(_, args, {getDb} : IContext,) => {
   const db = await getDb()
 
   const {
@@ -23,7 +15,7 @@ export const saveBloodGlucoseMeasurement = async (
     digestiveState,
     measurementDeviceModel,
     measuredAt,
-    deviceContext,
+    deviceContext
   } = args
 
   const newFormat = {
@@ -32,19 +24,21 @@ export const saveBloodGlucoseMeasurement = async (
     digestiveState,
     measurementDeviceModel,
     measuredAt,
-    deviceContext,
+    deviceContext
   }
-  const dinnerSituation = Object.entries(DigestiveStateLookup).find(
-    ([key, value]) => value === digestiveState,
-  )![0]
+  const dinnerSituation = Object
+    .entries(DigestiveStateLookup)
+    .find(([key, value]) => value === digestiveState,)![0]
   const convertGlucoseTypeToUSString = value => {
-    if (!value) return ''
+    if (!value) 
+      return ''
     return `${Math.round(value * 18)}`
   }
   let bgValue = ''
-  if (bloodGlucose.unit.toLowerCase() === 'mg/dl') bgValue = bloodGlucose.value
+  if (bloodGlucose.unit.toLowerCase() === 'mg/dl') 
+    bgValue = bloodGlucose.value
 
-  if (bloodGlucose.unit.toLowerCase() === 'mmol/l')
+  if (bloodGlucose.unit.toLowerCase() === 'mmol/l') 
     bgValue = convertGlucoseTypeToUSString(bloodGlucose.value)
 
   const oldFormat = {
@@ -54,53 +48,66 @@ export const saveBloodGlucoseMeasurement = async (
     createdAt: measuredAt,
     iGlucoseDataId: freshId(17), // this is forced to unique so this is a hack
   }
-  const objectToWrite = { ...oldFormat, ...newFormat }
-  const retVal = await db.collection('bloodglucoses').insertOne(objectToWrite)
+  const objectToWrite = {
+    ...oldFormat,
+    ...newFormat
+  }
+  const retVal = await db
+    .collection('bloodglucoses')
+    .insertOne(objectToWrite)
   const rz = retVal.ops[0]
-  const user = await db.collection('users').findOne({
-    _id: ObjectID.createFromHexString(patientId),
-    patientState: { $exists: 1 },
-  })
-  const mobile = user.username.replace('@ijk.com', '')
+  const user = await db
+    .collection('users')
+    .findOne({
+      _id: ObjectID.createFromHexString(patientId),
+      patientState: {
+        $exists: 1
+      }
+    })
+  const mobile = user
+    .username
+    .replace('@ijk.com', '')
   const nickname = user.nickname
   if (user.healthCareTeamId && user.healthCareTeamId.length > 0) {
     if (parseFloat(bgValue) >= 18 * 10) {
-      await db.collection('warnings').insertOne({
-        bloodglucoseId: rz._id, // 关联的测量记录
-        bgValue, // 本次记录的血糖值，转换后
-        warningType: 'HIGH_GLUCOSE', // 预备以后有别的警告记录种类
-        dinnerSituation,
-        patientId,
-        nickname,
-        gender: user.gender, // 'male/female',
-        dateOfBirth: user.dateOfBirth,
-        mobile,
-        createdAt: new Date(),
-        healthCareTeamId: user.healthCareTeamId[0],
-      })
+      await db
+        .collection('warnings')
+        .insertOne({
+          bloodglucoseId: rz._id, // 关联的测量记录
+          bgValue, // 本次记录的血糖值，转换后
+          warningType: 'HIGH_GLUCOSE', // 预备以后有别的警告记录种类
+          dinnerSituation,
+          patientId,
+          nickname,
+          gender: user.gender, // 'male/female',
+          dateOfBirth: user.dateOfBirth,
+          mobile,
+          createdAt: new Date(),
+          healthCareTeamId: user.healthCareTeamId[0]
+        })
     } else if (parseFloat(bgValue) < 18 * 4) {
-      await db.collection('warnings').insertOne({
-        bloodglucoseId: rz._id, // 关联的测量记录
-        bgValue, // 本次记录的血糖值，转换后
-        warningType: 'LOW_GLUCOSE', // 预备以后有别的警告记录种类
-        dinnerSituation,
-        patientId,
-        nickname,
-        gender: user.gender, // 'male/female',
-        dateOfBirth: user.dateOfBirth,
-        mobile,
-        createdAt: new Date(),
-        healthCareTeamId: user.healthCareTeamId[0],
-      })
+      await db
+        .collection('warnings')
+        .insertOne({
+          bloodglucoseId: rz._id, // 关联的测量记录
+          bgValue, // 本次记录的血糖值，转换后
+          warningType: 'LOW_GLUCOSE', // 预备以后有别的警告记录种类
+          dinnerSituation,
+          patientId,
+          nickname,
+          gender: user.gender, // 'male/female',
+          dateOfBirth: user.dateOfBirth,
+          mobile,
+          createdAt: new Date(),
+          healthCareTeamId: user.healthCareTeamId[0]
+        })
     }
   }
-  return !!retVal.result.ok ? retVal.insertedId : null
+  return !!retVal.result.ok
+    ? retVal.insertedId
+    : null
 }
-export const saveBloodGlucoseMeasurementNew = async (
-  _,
-  args,
-  { getDb }: IContext,
-) => {
+export const saveBloodGlucoseMeasurementNew = async(_, args, {getDb} : IContext,) => {
   const db = await getDb()
   const {
     bloodGlucoseValue,
@@ -109,7 +116,7 @@ export const saveBloodGlucoseMeasurementNew = async (
     patientId,
     measurementTime,
     deviceInformation,
-    measuredAt = new Date(),
+    measuredAt = new Date()
   } = args
   const measureTimeChinese = [
     '早餐前',
@@ -119,7 +126,7 @@ export const saveBloodGlucoseMeasurementNew = async (
     '晚餐前',
     '晚餐后',
     '睡前',
-    '凌晨',
+    '凌晨'
   ]
   const measureTimeEng = [
     'BEFORE_BREAKFAST',
@@ -129,7 +136,7 @@ export const saveBloodGlucoseMeasurementNew = async (
     'BEFORE_DINNER',
     'AFTER_DINNER',
     'BEFORE_SLEEPING',
-    'MIDNIGHT',
+    'MIDNIGHT'
   ]
   const objFirst = {
     bloodGlucoseValue,
@@ -138,7 +145,7 @@ export const saveBloodGlucoseMeasurementNew = async (
     patientId,
     measurementTime,
     deviceInformation,
-    measuredAt,
+    measuredAt
   }
   const frId = new ObjectID()
   const objSecond = {
@@ -148,56 +155,53 @@ export const saveBloodGlucoseMeasurementNew = async (
     labels: [],
     dataStatus: 'ACTIVE',
     createdAt: measuredAt,
-    updatedAt: measuredAt,
+    updatedAt: measuredAt
   }
 
-  const objectToWrite = { ...objFirst, ...objSecond }
-  const retVal = await db.collection('bloodGlucoses').insertOne(objectToWrite)
-
-  // 餐前血糖测量后添加定时事件
-  // 只有自动测量和餐前才添加定时事件
-  // 添加事件前清除这个患者之前的事件
-  // 事件key：pigeon_bg_<patientId>_BEFORE_BREAKFAST_午餐前_hh:mm
-
-  if (inputType === 'DEVICE') {
-    if (
-      measurementTime.indexOf('BEFORE') > -1 &&
-      measurementTime != 'BEFORE_SLEEPING'
-    ) {
-      console.log('-----------add reids cron job----------------')
-      const key = `bg_${patientId}_${measurementTime}_${
-        measureTimeChinese[measureTimeEng.indexOf(measurementTime)]
-      }_${moment().format('HH:mm')}`
-      const querykey = `bg_${patientId}`
-      const existKeys = await queryDelayEvent(querykey)
-      console.log('*****', existKeys)
-      if (existKeys && existKeys.length > 0) {
-        existKeys.forEach(async element => {
-          await deleteDelayEvent(element)
-        })
-      }
-      await addDelayEvent(key, 60 * 60 * 2)
-    }
+  const objectToWrite = {
+    ...objFirst,
+    ...objSecond
   }
+  const retVal = await db
+    .collection('bloodGlucoses')
+    .insertOne(objectToWrite)
+
+  // 餐前血糖测量后添加定时事件 只有自动测量和餐前才添加定时事件 添加事件前清除这个患者之前的事件
+  // 事件key：pigeon_bg_<patientId>_BEFORE_BREAKFAST_午餐前_hh:mm if (inputType ===
+  // 'DEVICE') {   if (     measurementTime.indexOf('BEFORE') > -1 &&
+  // measurementTime != 'BEFORE_SLEEPING'   ) {     console.log('-----------add
+  // reids cron job----------------')     const key =
+  // `bg_${patientId}_${measurementTime}_${
+  // measureTimeChinese[measureTimeEng.indexOf(measurementTime)]
+  // }_${moment().format('HH:mm')}`     const querykey = `bg_${patientId}` const
+  // existKeys = await queryDelayEvent(querykey)     console.log('*****',
+  // existKeys)     if (existKeys && existKeys.length > 0) {
+  // existKeys.forEach(async element => {         await deleteDelayEvent(element)
+  //     })     }     await addDelayEvent(key, 60 * 60 * 2)   } }
 
   const rz = retVal.ops[0]
-  const user = await db.collection('users').findOne({
-    _id: ObjectID.createFromHexString(patientId),
-    patientState: { $exists: 1 },
-  })
+  const user = await db
+    .collection('users')
+    .findOne({
+      _id: ObjectID.createFromHexString(patientId),
+      patientState: {
+        $exists: 1
+      }
+    })
   if ('NEEDLE_BG1' === bloodGlucoseDataSource && user && !user.isUseBg1) {
-    await db.collection('users').update(
-      {
-        _id: user._id,
-      },
-      {
+    await db
+      .collection('users')
+      .update({
+        _id: user._id
+      }, {
         $set: {
-          isUseBg1: true,
-        },
-      },
-    )
+          isUseBg1: true
+        }
+      },)
   }
-  const mobile = user.username.replace('@ijk.com', '')
+  const mobile = user
+    .username
+    .replace('@ijk.com', '')
   const nickname = user.nickname
   if (user.healthCareTeamId && user.healthCareTeamId.length > 0) {
     const isTooDamnHigh = parseFloat(bloodGlucoseValue) >= 18 * 10
@@ -206,8 +210,7 @@ export const saveBloodGlucoseMeasurementNew = async (
       const warning = {
         bloodglucoseId: rz._id, // 关联的测量记录
         bgValue: bloodGlucoseValue, // 本次记录的血糖值，转换后
-        dinnerSituation:
-          measureTimeChinese[measureTimeEng.indexOf(measurementTime)],
+        dinnerSituation: measureTimeChinese[measureTimeEng.indexOf(measurementTime)],
         patientId,
         nickname,
         gender: user.gender, // 'male/female',
@@ -215,78 +218,87 @@ export const saveBloodGlucoseMeasurementNew = async (
         mobile,
         createdAt: new Date(),
         healthCareTeamId: user.healthCareTeamId[0],
-        warningType: '',
+        warningType: ''
       }
       if (isTooDamnHigh) {
-        console.log('high', bloodGlucoseValue)
+        // console.log('high', bloodGlucoseValue)
         warning.warningType = 'HIGH_GLUCOSE'
       } else if (isTooDamnLow) {
-        console.log('low', bloodGlucoseValue)
+        // console.log('low', bloodGlucoseValue)
         warning.warningType = 'LOW_GLUCOSE'
       }
-      await db.collection('warnings').insertOne(warning)
-      pubsub.publish('warningAdded', { warningAdded: warning })
+      await db
+        .collection('warnings')
+        .insertOne(warning)
+      pubsub.publish('warningAdded', {warningAdded: warning})
     }
   }
-  return !!retVal.result.ok ? retVal.insertedId : null
+  return !!retVal.result.ok
+    ? retVal.insertedId
+    : null
 }
 
-export const updateRemarkOfBloodglucoses = async (
-  _,
-  args,
-  { getDb }: IContext,
-) => {
+export const updateRemarkOfBloodglucoses = async(_, args, {getDb} : IContext,) => {
   const db = await getDb()
 
-  const { _id, remark } = args
+  const {_id, remark} = args
 
   const retVal = await db
     .collection('bloodglucoses')
-    .update({ _id: maybeCreateFromHexString(_id) }, { $set: { remark } })
+    .update({
+      _id: maybeCreateFromHexString(_id)
+    }, {$set: {
+        remark
+      }})
   return !!retVal.result.ok
 }
-export const updateRemarkOfBloodglucosesNew = async (
-  _,
-  args,
-  { getDb }: IContext,
-) => {
+export const updateRemarkOfBloodglucosesNew = async(_, args, {getDb} : IContext,) => {
   const db = await getDb()
 
-  const { _id, remark, updatedAt } = args
+  const {_id, remark, updatedAt} = args
 
   const retVal = await db
     .collection('bloodGlucoses')
-    .update({ _id: String(_id) }, { $set: { note: remark, updatedAt } })
+    .update({
+      _id: String(_id)
+    }, {
+      $set: {
+        note: remark,
+        updatedAt
+      }
+    })
   return !!retVal.result.ok
 }
 
-export const deleteOfBloodglucoses = async (_, args, { getDb }: IContext) => {
+export const deleteOfBloodglucoses = async(_, args, {getDb} : IContext) => {
   const db = await getDb()
 
-  const { _id } = args
+  const {_id} = args
   const retValue = await db
     .collection('bloodglucoses')
-    .deleteOne({ _id: maybeCreateFromHexString(_id) })
+    .deleteOne({_id: maybeCreateFromHexString(_id)})
   await db
     .collection('warnings')
-    .deleteOne({ bloodglucoseId: maybeCreateFromHexString(_id) })
+    .deleteOne({bloodglucoseId: maybeCreateFromHexString(_id)})
   return !!retValue.result.ok
 }
 
-export const logicalDeleteOfBloodglucoses = async (
-  _,
-  args,
-  { getDb }: IContext,
-) => {
+export const logicalDeleteOfBloodglucoses = async(_, args, {getDb} : IContext,) => {
   const db = await getDb()
 
-  const { _id } = args
+  const {_id} = args
   const retValue = await db
     .collection('bloodGlucoses')
-    .updateOne({ _id }, { $set: { dataStatus: 'DELETED' } })
+    .updateOne({
+      _id
+    }, {
+      $set: {
+        dataStatus: 'DELETED'
+      }
+    })
 
   await db
     .collection('warnings')
-    .deleteOne({ bloodglucoseId: maybeCreateFromHexString(_id) })
+    .deleteOne({bloodglucoseId: maybeCreateFromHexString(_id)})
   return !!retValue.result.ok
 }
