c8d273256d38a7c11f9e2424f9cc377a5032db61
Sirius Guo <guotuo1024@gmail.com>
2018-07-25 16:31:10 +0800

    fix appointment note query

--- src/queries/chatMessages.js
+++ src/queries/chatMessages.js
@@ -1,9 +1,26 @@
 export const chatMessages = async (_, args, { getDb }) => {
   const db = await getDb()
 
+  const appointment = (await db.collection('appointments').find({
+    _id: args.appointmentId
+  }).toArray())[0];
+
+  // console.log(appointment)
+
+  const appointments = await db.collection('appointments').find({
+    patientId: appointment.patientId
+  }).toArray();
+
+
+
+
   const messages = await db
     .collection('chatMessages')
-    .find({ appointmentId: args.appointmentId })
+    .find({
+      appointmentId: {
+        $in: appointments.map(a => { return a._id })
+      }
+    })
     .sort({ sentAt: 1 })
     .toArray()
 
