d9e4733c7b97abf9bdcdfacc2d18e9ec8efb5196
KaroseLiu <kusoroselh@gmail.com>
2018-07-27 11:38:23 +0800

    Add the send massText

--- src/restful/router/index.js
+++ src/restful/router/index.js
@@ -1,10 +1,13 @@
 const Router = require('koa-router')
 const restfulRouter = new Router()
 import { uploadFileByType } from '../fileUpload/index'
+
 import { wechatPayServices } from '../../wechatPay'
 
 import { wechatPayment, payNotify } from '../../wechatPay'
 import { aliPayNotify } from '../../alipay/nofity'
+
+import { sendMassText } from '../massText'
 
 restfulRouter.post('/uploadFile', async ctx => {
   const result = await uploadFileByType(ctx)
@@ -23,12 +26,17 @@ restfulRouter.get('/wechatSandbox', async ctx => {
   })
   ctx.body = result
 })
+
 restfulRouter.get('/wechatSandbox/query', async ctx => {
   const { orderId } = ctx.query
   const result = await wechatPayServices.queryUnifiedOrder({
     orderId,
     type: 'out_trade_no',
   })
+})
+
+restfulRouter.post('/sendMassText', async ctx => {
+  const result = await sendMassText(ctx)
   ctx.body = result
 })
 
---
+++ src/wechatPay/goods.js
@@ -0,0 +1,28 @@
+const good = {
+  _id: '1212',
+  type: 'yearService',
+  name: '试纸包年装(含首年服务费)',
+  price: 499,
+  feeType: 'CNY',
+  couponFee: 200,
+  actualPrice: 299,
+  desc:
+    '加入东方医院糖尿病共同照护门诊的前500位患者将会以每年299元的价格享受价值499元的糖尿病共同照护服务，主要包括：iHealth血糖仪一台、试纸200条以及一年的院外跟踪服务。',
+  goodStatus: 'SALE',
+  details: [
+    {
+      name: '血糖仪',
+      type: 'bg1',
+    },
+    {
+      name: '试纸200条',
+      type: 'testPaper',
+    },
+    {
+      name: '一年的院外跟踪服务',
+      type: 'yearServices',
+    },
+  ],
+  publishAt: new Date(),
+  createdAt: new Date(),
+}
