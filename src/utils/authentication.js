4104accaedde3f22d60bc3c4a85c5b4928643b4c
liuhuan <liuhuan358@sina.com>
2018-08-21 10:04:46 +0800

    add achievement share confirm

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,22 +1,25 @@
-import {GraphQLError} from 'graphql';
+import { GraphQLError } from 'graphql';
 
-let {AUTH} = process.env
-if (!AUTH) 
+let { AUTH } = process.env
+if (!AUTH)
   AUTH = 'FALSE';
 
 export const userAuth = user => {
   if (!user) {
-    if (AUTH === 'TRUE') 
-      throw new GraphQLError('AuthenticationError',);
-    }
+    if (AUTH === 'TRUE')
+      throw new GraphQLError('AuthenticationError');
   }
+}
 
-export const authForApp = (requestType, funcName, func) => async(rootValue, args, ctx,) => {
+export const authForApp = (requestType, funcName, func) => async (rootValue, args, ctx, ) => {
+  if (funcName !== 'saveFoodContents') {
+    console.log(`${requestType}: Calling ${funcName} with args ${JSON.stringify(args)}`)
+  }
   if (['loginOrSignUp', 'wechatLoginOrSignUp', 'professionalLoginForWechat', 'professionalLoginForMobile'].indexOf(funcName) == -1) {
     if (!ctx.userInfo) {
-      if (AUTH === 'TRUE') 
-        throw new GraphQLError('AuthenticationError',);
-      }
+      if (AUTH === 'TRUE')
+        throw new GraphQLError('AuthenticationError');
     }
+  }
   return func(rootValue, args, ctx)
 }
