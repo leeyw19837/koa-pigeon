2b44da275d3f953fb9f466cee6251a29cc234c39
蒋凯 <baidan4855@qq.com>
2018-09-19 19:32:28 +0800

    solve the incorrect timeout of session

--- src/modules/delayJob/index.js
+++ src/modules/delayJob/index.js
@@ -13,5 +13,8 @@ export const setDelayJob = (id, job, sec) => {
   if (isJobExists(id)) {
     delDelayJob(id)
   }
-  jobs[id] = setTimeout(job, sec * 1000).toString()
+  jobs[id] = setTimeout(() => {
+    job()
+    delDelayJob(id)
+  }, sec * 1000).toString()
 }
