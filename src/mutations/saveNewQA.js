adba566dd3a6d7e6f0829ce1339b7d4599af0257
Sirius Guo <guotuo1024@gmail.com>
2018-09-19 15:41:07 +0800

    Fix QA collection struct

--- src/mutations/saveNewQA.js
+++ src/mutations/saveNewQA.js
@@ -1,16 +1,31 @@
 import freshId from 'fresh-id'
+import { ObjectId } from 'mongodb'
 
 export const saveNewQA = async (_, args, context) => {
   const db = await context.getDb()
   // 把qa保存起来
   const { q, a, msgId, cde } = args
+
+  // query sender detail information
+  const msg = await db.collection('needleChatMessages').findOne({ _id: msgId })
+  const sender = await db
+    .collection('users')
+    .findOne({ _id: ObjectId.createFromHexString(msg.senderId) })
+  // query hospital
+  const healthCareTeam = await db.collection('healthCareTeams').findOne({
+    _id: sender.healthCareTeamId[0],
+  })
+
   await db.collection('aiChatQA').insertOne({
     _id: freshId(),
     q,
     a,
+    patientId: msg.senderId,
+    questioner: sender.nickname,
+    hospital: healthCareTeam.institutionName,
     msgId,
     cde,
-    approved: false,
+    approved: 0,
     approvedUser: null,
     createdAt: new Date(),
     approvedAt: null,
