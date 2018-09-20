88cf232d25c5c9933addc779e2f700db58e56b28
Sirius Guo <guotuo1024@gmail.com>
2018-09-20 18:26:53 +0800

    Add QAList query implement

---
+++ src/queries/getQAList.js
@@ -0,0 +1,36 @@
+export const getQAList = async (_, args, { getDb }) => {
+  const db = await getDb()
+  const condition = {}
+  switch (args.approved) {
+    case 0:
+      condition = { createdAt: 1 }
+      break
+    case 1:
+      condition = { createdAt: -1 }
+      break
+  }
+  const { pageSize, pageIndex } = args
+
+  const recordsCount = await db
+    .collection('aiChatQA')
+    .count({ approved: args.approved })
+
+  const totalPage = Math.ceil(recordsCount / pageSize)
+  pageIndex = totalPage > pageIndex ? totalPage : pageIndex
+
+  const records = await db
+    .collection('aiChatQA')
+    .find({
+      approved: args.approved,
+    })
+    .sort(condition)
+    .skip((pageIndex - 1) * pageSize) // 忽略前n页
+    .limit(pageSize) // 拿pageSize条数
+
+  return {
+    totalPage,
+    pageIndex,
+    pageSize,
+    records,
+  }
+}
