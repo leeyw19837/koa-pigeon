64e87ee389338d4eceee2290f3349d76ba13d509
leeyw19837 <leeyw19837@hotmail.com>
2018-09-27 18:20:50 +0800

    add appointment related interfaces

--- src/mutations/appointment.js
+++ src/mutations/appointment.js
@@ -1,7 +1,83 @@
 import freshId from 'fresh-id'
+import {ObjectID} from "mongodb";
 const moment = require('moment')
 
 export const addAppointment = async (_, args, context) => {
   context.response.set('effect-types', 'Appointment,DailyAppointment')
   return true
 }
+
+export const updateAppointmentInfos = async (_, params, context) => {
+  const { patientId, nickname, mobile, source, expectedTime, note } = params
+  const $setObj = {
+    nickname,
+    mobile,
+    source,
+    expectedTime,
+    note
+  }
+  await db.collection('appointments').update(
+    {
+      patientId,
+    },
+    {
+      $set: $setObj,
+    },
+  )
+  return true
+}
+
+export const updateAppointmentDetailInfos = async (_, params, context) => {
+  const {
+    patientId,
+    hisNumber,
+    blood,
+    footAt,
+    eyeGroundAt,
+    insulinAt,
+    nutritionAt,
+    healthTech,
+    quantizationAt
+  } = params
+  const $setObj = {
+    hisNumber,
+    blood,
+    footAt,
+    eyeGroundAt,
+    insulinAt,
+    nutritionAt,
+    healthTech,
+    quantizationAt
+  }
+  await db.collection('appointments').update(
+    {
+      patientId,
+    },
+    {
+      $set: $setObj,
+    },
+  )
+  return true
+}
+
+export const deletePatientAppointment = async (_, params, context) => {
+  const { patientId } = params
+  await db.collection('appointments').deleteOne(
+    {
+      patientId,
+    },
+  )
+  return true
+}
+
+export const addPatientAppointment = async (_, params, context) => {
+  console.log('addPatientAppointment',params)
+  // await db.collection('appointments').deleteOne(
+  //   {
+  //     patientId,
+  //   },
+  // )
+  context.response.set('effect-types', 'PatientList')
+  return true
+}
+
