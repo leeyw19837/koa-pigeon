449d96d6d32cdea2fb02ca38cec9e01815b71f6f
leeyw19837 <leeyw19837@hotmail.com>
2018-10-31 19:31:19 +0800

    new requirement: add membership

--- src/queries/getGoods.js
+++ src/queries/getGoods.js
@@ -4,3 +4,8 @@ export const getGoods = async (_, args) => {
   const { goodId, goodType } = args
   return findGoodById({ goodId, goodType })
 }
+
+export const getAllGoods = async (_, args) => {
+  return await db.collection('goods').find({}).toArray()
+}
+
