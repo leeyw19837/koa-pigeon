69c8906c1301ee469085f32ba8bbb2acbc1b416a
thiswhy <1414770848@qq.com>
2018-11-16 10:12:52 +0800

    add archived info api

--- src/mutations/setArchived.js
+++ src/mutations/setArchived.js
@@ -1,41 +1,49 @@
-import { ObjectId } from 'mongodb'
-import { createQuarterReplaceAddition } from './appointment';
+import {ObjectId} from 'mongodb'
+import {createQuarterReplaceAddition} from './appointment';
+import {get} from 'lodash'
 
 export const setArchived = async (_, params, context) => {
-  const { patientId, archivedReason } = params
+  const {patientId, archivedReason, archivedInfo} = params
 
   const userObjectId = ObjectId.createFromHexString(patientId)
 
   const patient = await db
     .collection('users')
-    .findOne({ _id: userObjectId })
+    .findOne({_id: userObjectId})
 
   if (!patient) {
     throw new Error('Patient is not existed!')
   }
 
-  const appointments = await db.collection('appointments').find({ patientId: patientId, isOutPatient: false }).toArray()
+  const appointments = await db.collection('appointments').find({patientId: patientId, isOutPatient: false}).toArray()
+  const user = await db.collection('users').findOne({_id: userObjectId})
+  const archivedInfos = get(user, "archivedInfos", [])
+  archivedInfos.unshift({...archivedInfo, "archivedDate": [new Date()]})
   await db
     .collection('users')
     .update({
       _id: userObjectId
     }, {
-        $set: {
-          patientState: 'ARCHIVED',
-          archivedReason: archivedReason,
-        }
-      })
+      $set: {
+        patientState: 'ARCHIVED',
+        archivedReason: archivedReason,
+        archivedInfos
+      }
+    })
 
   await db
     .collection('appointments')
     .update({
-      _id: {
-        $in: appointments.map(a => { return a._id })
-      }
-    }, {
+        _id: {
+          $in: appointments.map(a => {
+            return a._id
+          })
+        }
+      }, {
         $set: {
           patientState: 'ARCHIVED',
           archivedReason: archivedReason,
+          archivedInfos
         },
       }, {
         multi: true,
@@ -45,13 +53,16 @@ export const setArchived = async (_, params, context) => {
   await db
     .collection('treatmentState')
     .update({
-      _id: {
-        $in: appointments.map(a => { return a.treatmentStateId })
-      }
-    }, {
+        _id: {
+          $in: appointments.map(a => {
+            return a.treatmentStateId
+          })
+        }
+      }, {
         $set: {
           patientState: 'ARCHIVED',
           archivedReason: archivedReason,
+          archivedInfos
         },
       }, {
         multi: true,
@@ -61,13 +72,19 @@ export const setArchived = async (_, params, context) => {
   await db
     .collection('outpatients')
     .update({
-      state: 'WAITING',
-      appointmentsId: {
-        $in: appointments.map(a => { return a._id })
-      }
-    }, {
+        state: 'WAITING',
+        appointmentsId: {
+          $in: appointments.map(a => {
+            return a._id
+          })
+        }
+      }, {
         $pull: {
-          appointmentsId: { $in: appointments.map(a => { return a._id }) },
+          appointmentsId: {
+            $in: appointments.map(a => {
+              return a._id
+            })
+          },
           patientsId: patientId,
         },
         $set: {
@@ -86,12 +103,12 @@ export const setArchived = async (_, params, context) => {
 }
 
 export const unsetArchived = async (_, params, context) => {
-  const { patientId, outpatientId, appointmentTime } = params
+  const {patientId, outpatientId, appointmentTime} = params
 
   const userObjectId = ObjectId.createFromHexString(patientId)
   const patient = await db
     .collection('users')
-    .findOne({ _id: userObjectId })
+    .findOne({_id: userObjectId})
 
   if (!patient) {
     throw new Error('Patient is not existed!')
@@ -102,12 +119,12 @@ export const unsetArchived = async (_, params, context) => {
     .update({
       _id: userObjectId
     }, {
-        $set: {
-          patientState: 'ACTIVE',
-        }
-      })
+      $set: {
+        patientState: 'ACTIVE',
+      }
+    })
 
-  await createQuarterReplaceAddition(null, { patientId, outpatientId, appointmentTime })
+  await createQuarterReplaceAddition(null, {patientId, outpatientId, appointmentTime})
 
   context
     .response
