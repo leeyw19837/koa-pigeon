06d2b652cbf708fdd3dc8eaa6f9392b2156751dd
leeyw19837 <leeyw19837@hotmail.com>
2018-12-19 14:40:51 +0800

    modify: bg1 not use reason

--- src/queries/getUserUseBg1Situation.js
+++ src/queries/getUserUseBg1Situation.js
@@ -6,58 +6,58 @@ export const getUserUseBg1Situation = async (_, args, { getDb }) => {
         useBg1Situation: false
     }
 
-  const user = await db
-      .collection('users')
-      .find({
-          _id:ObjectID.createFromHexString(args.patientId)
-      })
-      .toArray()
+  // const user = await db
+  //     .collection('users')
+  //     .find({
+  //         _id:ObjectID.createFromHexString(args.patientId)
+  //     })
+  //     .toArray()
+  //
+  // if (user && user.length > 0) {
+  //   if (user.isUseBg1) {
+  //     if (user.isUseBg1 === true) {
+  //       if (!user.notUseBg1Reason) {
+  //         result.useBg1Situation = true
+  //       } else {
+  //         result.useBg1Situation = false
+  //       }
+  //     } else {
+  //       if (user.notUseBg1Reason && user.notUseBg1Reason === 'gotButNoUse') {
+  //         result.useBg1Situation = true
+  //       } else {
+  //         result.useBg1Situation = false
+  //       }
+  //     }
+  //   } else {
+  //     if (!user.notUseBg1Reason) {
+  //       result.useBg1Situation = true
+  //     } else {
+  //       result.useBg1Situation = false
+  //     }
+  //   }
+  // } else {
+  //   result.useBg1Situation = false
+  // }
 
-  if (user && user.length > 0) {
-    if (user.isUseBg1) {
-      if (user.isUseBg1 === true) {
-        if (!user.notUseBg1Reason) {
-          result.useBg1Situation = true
-        } else {
-          result.useBg1Situation = false
-        }
-      } else {
-        if (user.notUseBg1Reason && user.notUseBg1Reason === 'gotButNoUse') {
-          result.useBg1Situation = true
-        } else {
-          result.useBg1Situation = false
-        }
-      }
-    } else {
-      if (!user.notUseBg1Reason) {
+  const userBG1NotUseReasons = await db
+    .collection('BG1NotUseReason')
+    .find({
+        _id:args.patientId
+    })
+    .sort({createdAt:-1})
+    .toArray()
+
+  if (userBG1NotUseReasons && userBG1NotUseReasons.length>0){
+      const latestReason = userBG1NotUseReasons[0]
+      const { isGotBG1, isUseBG1, reason } = latestReason
+
+      if (isGotBG1 && !isUseBG1 && reason && reason!==''){
         result.useBg1Situation = true
       } else {
         result.useBg1Situation = false
       }
-    }
-  } else {
-    result.useBg1Situation = false
   }
 
-    // const userCount = await db
-    //     .collection('users')
-    //     .find({
-    //         _id:ObjectID.createFromHexString(args.patientId),
-    //         $or:[
-    //             {
-    //                 isUseBg1:true,
-    //             },
-    //             {
-    //                 $and:[
-    //                     {$or:[{isUseBg1:false},{isUseBg1:{$exists:false}}]},{notUseBg1Reason:{$exists:true}},{notUseBg1Reason:'gotButNoUse'}
-    //                 ]
-    //             }
-    //         ]
-    //     })
-    //     .count()
-
-    // result.useBg1Situation = (userCount !== 0)
-
-    console.log('getUserUseBg1Situation called! result',result)
-    return result
+  console.log('getUserUseBg1Situation called! result',result)
+  return result
 }
