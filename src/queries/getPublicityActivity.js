40a11e3ec7a4282442488f638717317b42946684
thiswhy <1414770848@qq.com>
2019-01-16 17:46:21 +0800

    add query publish activity

--- src/queries/getPublicityActivity.js
+++ src/queries/getPublicityActivity.js
@@ -3,3 +3,10 @@ export const getPublicityActivityController = async (_, args, context) => {
   // 找到当前用户的活动数据
   return await db.collection('publicityActivityController').findOne({patientId: args.patientId});
 }
+
+// 根据活动ID获取当前的活动信息
+export const getOneOfPublicityActivity = async (_, args, context) => {
+  const db = await context.getDb();
+  return await db.collection('publicityActivity').findOne({_id:args.activityId});
+}
+
