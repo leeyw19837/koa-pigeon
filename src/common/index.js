28b4c292224c03e84c7bb631d4d2ecc15caba702
KaroseLiu <kusoroselh@gmail.com>
2019-01-24 16:18:33 +0800

    Add the es_log

--- src/common/index.js
+++ src/common/index.js
@@ -1,7 +1,5 @@
 import 'pinyin4js'
-import get from 'lodash/get'
 import { send } from 'righteous-raven'
-const request = require('request-promise')
 
 export * from './formatBgValue'
 
@@ -9,8 +7,6 @@ const {
   RIGHTEOUS_RAVEN_URL,
   RIGHTEOUS_RAVEN_ID,
   RIGHTEOUS_RAVEN_KEY,
-  ELASTIC_ELEPHANT_URL = 'http://172.16.0.69:6010',
-  NODE_ENV,
 } = process.env
 
 export const sendTxt = async options => {
@@ -29,35 +25,4 @@ export const sendTxt = async options => {
   }
 }
 
-const sendToElasticSearch = ({ context, ...restInfo }) => {
-  const authorization = get(context, 'request.headers.authorization', '')
-  const isProd = NODE_ENV === 'production'
-  if (isProd) {
-    try {
-      const uri = `${ELASTIC_ELEPHANT_URL}/addLog`
-      const options = {
-        method: 'POST',
-        uri,
-        json: true,
-        headers: {
-          Authorization: authorization,
-          'Client-Label': 'pigeon',
-        },
-        body: restInfo,
-      }
-      request(options)
-    } catch (error) {
-      console.log('add Log error')
-    }
-  }
-}
-
-export const logger = {
-  log: params => {
-    try {
-      sendToElasticSearch(params)
-    } catch (error) {
-      console.log('logger error')
-    }
-  },
-}
+export * from './logger'
---
+++ src/common/logger.js
@@ -0,0 +1,47 @@
+import get from 'lodash/get'
+import { sign } from 'jsonwebtoken'
+const request = require('request-promise')
+
+const {
+  ELASTIC_ELEPHANT_URL = 'http://172.16.0.69:6010',
+  NODE_ENV,
+  JWT_SECRET,
+} = process.env
+
+const sendToElasticSearch = ({ context, ...restInfo }) => {
+  let authorization = get(context, 'request.headers.authorization', '')
+  if (!authorization) {
+    authorization = `Bearer ${sign({ from: 'pigeon' }, JWT_SECRET, {
+      expiresIn: '1m',
+    })}`
+  }
+  const isProd = NODE_ENV === 'production'
+  if (true) {
+    try {
+      const uri = `${ELASTIC_ELEPHANT_URL}/addLog`
+      const options = {
+        method: 'POST',
+        uri,
+        json: true,
+        headers: {
+          Authorization: authorization,
+          'Client-Label': 'pigeon',
+        },
+        body: restInfo,
+      }
+      request(options)
+    } catch (error) {
+      console.log('add Log error')
+    }
+  }
+}
+
+export const logger = {
+  log: params => {
+    try {
+      sendToElasticSearch(params)
+    } catch (error) {
+      console.log('logger error')
+    }
+  },
+}
