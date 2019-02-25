73f28d6d16d0f78852553a1ed0978d3f2d0aabc7
KaroseLiu <kusoroselh@gmail.com>
2019-02-25 10:02:45 +0800

    Add the configuration-content

--- src/queries/configurationContent.js
+++ src/queries/configurationContent.js
@@ -1,7 +1,13 @@
-
 export const configurationContent = async (_, args, context) => {
   const db = await context.getDb()
   const { type } = args
   const result = await db.collection('configurationContent').findOne({ type })
   return result
+}
+
+export const getConfigByCategory = async (_, { category }, { getDb }) => {
+  const db = await getDb()
+  return db.collection('configurationContent').findOne({
+    category,
+  })
 }
