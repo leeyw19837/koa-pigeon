2b6fc186456b6f08d79dc1e451c4c7d9cfdf0cb7
蒋凯 <baidan4855@qq.com>
2019-02-25 08:48:51 +0800

    pub new patient to outpatient immediately

--- src/mutations/wildPatient.js
+++ src/mutations/wildPatient.js
@@ -1,6 +1,11 @@
 import freshId from 'fresh-id'
+import { movePatientToOutpatientPlan } from './outpatientPlan'
 
-export const addWildPatient = async (_, { operatorId, patient }, context) => {
+export const addWildPatient = async (
+  _,
+  { operatorId, patient, planId },
+  context,
+) => {
   const db = await context.getDb()
   const { mobile, idCard } = patient
   const isDuplicate = await db.collection('wildPatients').count({
@@ -8,14 +13,18 @@ export const addWildPatient = async (_, { operatorId, patient }, context) => {
     idCard,
   })
   if (isDuplicate) throw new Error('No duplicate mobile or idCard allowed')
-
+  const patientId = freshId()
   const result = await db.collection('wildPatients').insert({
-    _id: freshId(),
+    _id: patientId,
     status: 'NOT_FIRST_VISIT',
     ...patient,
     createdAt: new Date(),
     createdBy: operatorId,
   })
-
+  await movePatientToOutpatientPlan(
+    null,
+    { patientId, toPlan: { _id: planId } },
+    context,
+  )
   return result.result.ok
 }
