0d99270c2aef4cbb50ded8da039ba9f11d8484a2
thiswhy <1414770848@qq.com>
2019-03-13 18:48:25 +0800

    add location coordinate to recommend hospital

--- src/queries/getHospitalMessage.js
+++ src/queries/getHospitalMessage.js
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
  * 查询未入组患者下面的医院介绍
  * */
 export const getHospitalMessage = async (_, args) => {
-  const { patientId } = args
+  const { patientId, coordinate } = args
   const user = await db
     .collection('users')
     .findOne({
@@ -26,14 +26,29 @@ export const getHospitalMessage = async (_, args) => {
       }).sort({ recommendDegree: 1 }).toArray()
     
   } else {
-    hospitalMessage = await db
+    // 如果没有用户的地理位置信息，默认北大医院的坐标
+    const { longitude = 116.380702, latitude = 39.931959 } = coordinate
+    const nearHospital = await db
+      .collection('institutions')
+      .findOne({
+        isPartner: true,
+        location:
+          {
+            $near: {
+              $geometry: { type: "Point", coordinates: [longitude, latitude] },
+            }
+          },
+      },)
+    
+    const hospitalList = await db
       .collection('institutions')
       .find({
         isPartner: true,
+        _id: { $ne: nearHospital._id }
       }, {
         password: 0,
       }).sort({ recommendDegree: 1 }).toArray()
+    hospitalMessage = [nearHospital, ...hospitalList]
   }
-  console.log("hospitalMessage", hospitalMessage)
   return hospitalMessage
 }
